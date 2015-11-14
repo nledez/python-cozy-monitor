fb34516b7be861ef8344c5c9db6b53e04a45109f
Nicolas Ledez <github@ledez.net>
2015-11-14 16:07:00 +0100

    Mock file path refactoring

--- tests/test_cozy_monitor.py
+++ tests/test_cozy_monitor.py
@@ -10,9 +10,9 @@ CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
 class PopenMock(object):
-    def __init__(self, text):
-        self.stdout = open('{}/cozy-monitor-outputs/{}'.format(CURRENT_PATH,
-                                                               text))
+    def __init__(self, command):
+        file_path = '{}/cozy-monitor-outputs/cozy-monitor-{}.txt'
+        self.stdout = open(file_path.format(CURRENT_PATH, command))
 
 
 class CozyMonitor(unittest.TestCase):
@@ -43,18 +43,18 @@ class CozyMonitor(unittest.TestCase):
             'files': 'up',
             'kresus': 'up',
         }
-        mock_subprocess.Popen.return_value = PopenMock('cozy-monitor-status.txt')
 
+        mock_subprocess.Popen.return_value = PopenMock('status')
         full_out = cozy_monitor.status()
         self.assertIsInstance(full_out, dict)
         self.assertEqual(full_out, expected)
 
-        mock_subprocess.Popen.return_value = PopenMock('cozy-monitor-status.txt')
+        mock_subprocess.Popen.return_value = PopenMock('status')
         app_up = cozy_monitor.status('files')
         self.assertIsInstance(app_up, str)
         self.assertEqual(app_up, 'up')
 
-        mock_subprocess.Popen.return_value = PopenMock('cozy-monitor-status.txt')
+        mock_subprocess.Popen.return_value = PopenMock('status')
         app_stopped = cozy_monitor.status('calendar')
         self.assertIsInstance(app_stopped, str)
         self.assertEqual(app_stopped, 'stopped')
